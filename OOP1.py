e5a53345bd322caf3e61cbb75da439aacb7082d0
jamilethgongora <jamileth.gongora10@gmail.com>
2018-07-04 13:35:35 -0400

    oop v2 more exercise

---
+++ OOP1.py
@@ -0,0 +1,39 @@
+class Coche():
+
+    def __init__(self):                          #constructor (self.)
+
+        self.__largoChasis=250                   #encapsuladas(__)
+        self.__anchoChasis=120
+        self.__ruedas=4                         
+        self.__enmarcha=False
+
+
+    def arrancar(self,arrancamos):
+        self.__enmarcha=arrancamos
+
+
+        if(self.__enmarcha):
+            return "El coche esta en marcha"
+        
+        else:
+
+            return "El coche esta parado"
+
+    
+    def estado(self):
+        print("El coche tiene ", self.__ruedas, "ruedas. Un ancho de ", self.__anchoChasis, "y un largo de ", 
+        self.__largoChasis)
+
+
+miCoche=Coche()
+print(miCoche.arrancar(True))
+
+(miCoche.estado())
+
+ 
+print(".....................A continuacion creamos el segundo objeto...........")
+
+miCoche2=Coche()    
+print(miCoche2.arrancar(False))
+miCoche2.ruedas=2 
+(miCoche2.estado())
